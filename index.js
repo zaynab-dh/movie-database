cc5a8b415aac007858b0fa0f502e25979cb00c0f
Zaynab Dhaybi <zaynab.dhaybi@gmail.com>
2021-03-29 23:55:28 +0300

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-
+const router = express.Router();
+const date = new Date();
+const hour = date.getHours();
+var seconds = date.getSeconds();
 app.get('/', (req, res) => {
     res.send('ok')
-  })
+  });
+app.get('/test', (req, res,next)=> {
+    res.send({status:200, message:"ok"})
+});
+app.get('/time', (req, res,next)=> {
+    res.send({status:200, message:hour +":"+ seconds});
+    
+});
   
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   });
+
+
+
