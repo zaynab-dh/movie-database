4d7773fe8057ade88bef8a765f1f6936dafc9eb8
Zaynab Dhaybi <zaynab.dhaybi@gmail.com>
2021-03-31 22:51:23 +0300

    step 8

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+app.use(express.json());
 const router = express.Router();
 const date = new Date();
 const hour = date.getHours();
 var seconds = date.getSeconds();
 const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+    { id: 1,title: 'Jaws', year: 1975, rating: 8 },
+    { id: 2,title: 'Avatar', year: 2009, rating: 7.8 },
+    { id: 3,title: 'Brazil', year: 1985, rating: 8 },
+    { id: 4,title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ]
 
 app.get('/', (req, res) => {
@@ -57,6 +58,37 @@ app.get('/movies/read/by-title', (req, res)=> {
     movies.sort((a, b) => (a.title > b.title) ? 1 : -1) 
     let movie = movies.map(({ title }) => title)
     res.send({status:200, data:movie}); 
+});
+app.get('/movies/read/id/:id', (req, res)=> {
+    const mov = movies.find(c=>c.id===parseInt(req.params.id));
+    if(!mov)res.send({status:404, error:true, message:'the movie id does not exist'});
+    res.send({status:200, data:mov}); 
+});
+app.get('/movies/add', (req, res)=> {
+    let ttl = req.query.title;
+    let yr = req.query.year;
+    let rt = req.query.rating;
+
+    if(ttl === '' || yr === '' || yr.length !== 4 || isNaN(yr))
+    {
+        res.send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'}) 
+    }
+    else
+    {
+        if (rt == '')
+        {
+            rt = 4;
+        }
+        const newMovie = {
+            id:movies.length+1,
+            title:ttl,
+            year:yr,
+            rating:rt,
+        };
+        movies.push(newMovie);
+        res.send({status:200, data:movies}); 
+    }
+   
 });
 app.get('/movies/update', (req, res)=> {
     res.send({status:200, message:"Hello, "});
